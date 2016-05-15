03cbeed6755aa9c216fb32ec6c4c3efd843f1c41
shockham <samuel.hockham@gmail.com>
2016-05-15 09:57:13 +0100

    fixed read endpoints

--- app.py
+++ app.py
@@ -51,12 +51,26 @@ def create():
 
 @app.route('/job/read')
 def read_all():
-    rows = db.session.query(Team, Job, Doctor).filter(Job.done is not None).all()
+    #rows = db.session.query(Team, Job, Doctor).filter(Job.done is None).all()
+    rows = Job.query.filter_by(done=None)
 
     json_jobs = []
     for job in rows:
         json_jobs.append({
-            'job_id': job.id
+            'id': job.id,
+            'team_id': job.team_id,
+            'patient_id': job.patient_id,
+            'urgency': job.urgency,
+            'creator_comment': job.creator_comment,
+            'doctor_comment': job.doctor_comment,
+            'bed': job.bed,
+            'ward': job.ward,
+            'location': job.location,
+            'creator_id': job.creator_id,
+            'creator_name': job.creator_name,
+            'acknowledged': job.acknowledged,
+            'done': job.done,
+            'created_at': job.created_at
             })
 
     return jsonify({ 'jobs' : json_jobs })
@@ -64,13 +78,66 @@ def read_all():
 
 @app.route('/job/read/<team_id>')
 def read(team_id):
-    jobs = Job.query.filter_by(team_id=team_id)
-    return jsonify({ 'success' : True })
+    rows = Job.query.filter_by(team_id=team_id, done=None)
+
+    json_jobs = []
+    for job in rows:
+        json_jobs.append({
+            'id': job.id,
+            'team_id': job.team_id,
+            'patient_id': job.patient_id,
+            'urgency': job.urgency,
+            'creator_comment': job.creator_comment,
+            'doctor_comment': job.doctor_comment,
+            'bed': job.bed,
+            'ward': job.ward,
+            'location': job.location,
+            'creator_id': job.creator_id,
+            'creator_name': job.creator_name,
+            'acknowledged': job.acknowledged,
+            'done': job.done,
+            'created_at': job.created_at
+            })
+
+    return jsonify({ 'jobs' : json_jobs })
 
 
 @app.route('/job/update/<id>')
 def update(id):
+    team_id = request.form['team_id']
+    patient_id = request.form['patient_id']
+    urgency = request.form['urgency']
+    creator_comment = request.form['creator_comment']
+    doctor_comment = request.form['doctor_comment']
+    bed = request.form['bed']
+    ward = request.form['ward']
+    location = request.form['location']
+    creator_name = request.form['creator_name']
+
     job = Job.query.get(id)
+
+    if team_id is not None:
+        job.team_id = team_id
+    if patient_id is not None:
+        job.patient_id = patient_id
+    if urgency is not None:
+        job.urgency = urgency
+    if creator_comment is not None:
+        job.creator_comment = creator_comment
+    if doctor_comment is not None:
+        job.doctor_comment = doctor_comment
+    if bed is not None:
+        job.bed = bed
+    if ward is not None:
+        job.ward = ward
+    if location is not None:
+        job.location = location
+    if creator_name is not None:
+        job.creator_name = creator_name
+
+    db.session.add(job)
+    db.session.commit()
+
     return jsonify({ 'success' : True })
 
 if not app.debug:
